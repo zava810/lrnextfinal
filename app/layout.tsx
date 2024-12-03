eb1fb9aafbe20f6e8e997a2fde8cc8311b41cc4b
viml kumar <vimalkg@gmail.com>
2024-12-03 18:42:44 +0530

    added woff2 files and related files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,12 @@
 "use client";
 import '@/app/ui/global.css';
 import React, { useState } from 'react';
-import { hin115_font, ing115_font, bangla115_font, telugu115_font, mlyalm115_font, tmil115_font } from '@/fonts';
+////binaryhoriontal115.woff2  hex115.woff2 binaryvertical115.woff2
+import {
+		hin115_font, ing115_font, bangla115_font, telugu115_font,
+		mlyalm115_font, tmil115_font, kannada115_font, odia115_font,
+		sinhala115_font, pnzabi115_font, guzrati115_font
+} from '@/fonts';
 // import { Metadata } from 'next';
 
 // export const metadata: Metadata = {
@@ -23,7 +28,12 @@ export default function RootLayout({ children, }: { children: React.ReactNode; }
       case '2': return bangla115_font ;
       case '3': return telugu115_font ;
       case '4': return mlyalm115_font ;
-      case '5': return tmil115_font ;
+      case '5': return tmil115_font ;      
+      case '6': return kannada115_font ;
+      case '7': return odia115_font ;
+      case '8': return sinhala115_font ;
+      case '9': return pnzabi115_font ;
+      case 'L': return guzrati115_font ;
       default: return ing115_font ;
     }
   }
@@ -32,12 +42,17 @@ export default function RootLayout({ children, }: { children: React.ReactNode; }
         <body className={`${selectedFont.className} antialiased`}>
         <div>
           <select onChange={(e) => handleFontChange(getFontClass(e.target.value))}>
-            <option value="0">iNg115 font</option>
-            <option value="1">Hin115 font</option>
-            <option value="2">baNgla115 font</option>
-            <option value="3">Telugu115 font</option>
-            <option value="4">TAmil115 font</option>
-            <option value="5">mAlAyalAm115 font</option>
+            <option value="0">iNg115_font</option>
+            <option value="1">hin115_font</option>
+            <option value="2">bangla115_font</option>
+            <option value="3">telugu115_font</option>
+            <option value="4">mlyalm115_font</option>
+            <option value="5">tmil115_font</option>
+            <option value="6">kannada115_font</option>
+            <option value="7">odia115_font</option>
+            <option value="8">sinhala115_font</option>
+            <option value="9">pnzabi115_font</option>
+            <option value="L">guzrati115_font</option>
           </select>
         </div>
           {children}
