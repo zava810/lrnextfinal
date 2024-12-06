1d2875bcf336ab858d1bb7913941cd62a14e9a1b
viml kumar <vimalkg@gmail.com>
2024-12-06 09:14:55 +0530

    used localfont.classname string

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import React, { useState } from 'react';
 import {
 		hin115_font, ing115_font, bangla115_font, telugu115_font,
 		mlyalm115_font, tmil115_font, kannada115_font, odia115_font,
-		sinhala115_font, pnzabi115_font, guzrati115_font
+		sinhala115_font, pnzabi115_font, guzrati115_font, binaryvertical115_font
 } from '@/fonts';
 // import { Metadata } from 'next';
 
@@ -19,29 +19,35 @@ import {
 // };
 export default function RootLayout({ children, }: { children: React.ReactNode; }) {
   // https://stackoverflow.com/questions/72946973/how-to-access-a-state-of-an-component-from-another-component-in-next-js
-  const [selectedFont, setSelectedFont] = useState(ing115_font);
-  const handleFontChange = (font: any) => { setSelectedFont(font); };
-  const getFontClass = (sval:string) => {
-    switch (sval) {
-      case '0': return ing115_font ;
-      case '1': return hin115_font ;
-      case '2': return bangla115_font ;
-      case '3': return telugu115_font ;
-      case '4': return mlyalm115_font ;
-      case '5': return tmil115_font ;      
-      case '6': return kannada115_font ;
-      case '7': return odia115_font ;
-      case '8': return sinhala115_font ;
-      case '9': return pnzabi115_font ;
-      case 'L': return guzrati115_font ;
-      default: return ing115_font ;
+  const [selectedFontcn, setSelectedFontcn] = useState(ing115_font.className);
+
+  const handleFontChange = (selectedval: string) => {
+    const getFontcn = (selectval:string) => {
+      switch (selectval) {
+        case '0': return ing115_font.className ;
+        case '1': return hin115_font.className ;
+        case '2': return bangla115_font.className ;
+        case '3': return telugu115_font.className;
+        case '4': return mlyalm115_font.className ;
+        case '5': return tmil115_font.className ;      
+        case '6': return kannada115_font.className ;
+        case '7': return odia115_font.className ;
+        case '8': return sinhala115_font.className ;
+        case '9': return pnzabi115_font.className ;
+        case 'L': return guzrati115_font.className ;
+        case 'J': return binaryvertical115_font.className ;
+        default: return ing115_font.className ;
+      }
     }
-  }
+    const selectvaluefontcn = getFontcn(selectedval);
+    setSelectedFontcn(selectvaluefontcn);
+  };
+
   return (
     <html lang="en">
-        <body className={`${selectedFont.className} antialiased`}>
-        <div>
-          <select onChange={(e) => handleFontChange(getFontClass(e.target.value))}>
+        <body className={`${selectedFontcn} antialiased`}>
+          <div>
+            <select onChange={(e) => handleFontChange(e.target.value)}>
             <option value="0">iNg115_font</option>
             <option value="1">hin115_font</option>
             <option value="2">bangla115_font</option>
@@ -53,6 +59,7 @@ export default function RootLayout({ children, }: { children: React.ReactNode; }
             <option value="8">sinhala115_font</option>
             <option value="9">pnzabi115_font</option>
             <option value="L">guzrati115_font</option>
+            <option value="L">binaryvertical115_font</option>
           </select>
         </div>
           {children}
